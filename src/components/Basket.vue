4f2335c1751f29dfaac3729ecba6c20ef267bf96
pratham karki <prathamkarki21@gmail.com>
2025-07-14 15:43:11 +0545

    lucide vue icons setup

---
+++ src/components/Basket.vue
@@ -0,0 +1,16 @@
+<template>
+    <section class="basket_container">
+      <div v-if="basketItems.length === 0">
+        <h1></h1>
+      </div>
+    </section>
+
+</template>
+
+
+<script setup>
+  const props = defineProps({
+    basketItems: Array,
+  });
+
+</script>
