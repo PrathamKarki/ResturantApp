182d9bcecb15d5f28ceb0c7b84d5bc1b772610e6
pratham karki <prathamkarki21@gmail.com>
2025-07-17 11:55:42 +0545

    initial setup for router

--- src/main.js
+++ src/main.js
@@ -3,4 +3,21 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { createWebHistory, createRouter } from 'vue-router'
+
+import CategoriesList from './components/CategoriesList.vue'
+import Checkout from './components/Checkout.vue'
+
+const routes = [
+  {path:'/', component: CategoriesList},
+  {path: '/Checkout', component: Checkout}
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+const app = createApp(App)
+app.use(router);
+app.mount('#app');
